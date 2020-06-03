09c2400b151d3b4613ead73f173daa12374f660b
Your Name <you@example.com>
2020-06-03 19:26:11 +0530

    app updated with shopage, collection preview, collection item

--- src/App.js
+++ src/App.js
@@ -2,19 +2,16 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HatsPage</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
