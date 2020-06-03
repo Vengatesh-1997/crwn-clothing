829662a6fd1a8a15c62fb79ea28c147058b9cf0a
Your Name <you@example.com>
2020-06-03 08:25:19 +0530

    broken everything into smaller components and added styles to it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Homepage from './pages/homepage/homepage.component'
 
 function App() {
   return (
-    <div yarn>Hello World</div>
+    <div><Homepage/></div>
   );
 }
 
